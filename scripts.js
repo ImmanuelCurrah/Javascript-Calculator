58cc9610b7f19490151d2058ccfd623baf54eeda
Immanuel Currah <immanueli.currah@gmail.com>
2022-05-03 12:39:18 -0400

    calc shows the button that is clicked

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
 // set the starting number to 0
 let number = document.getElementById("number");
 number.innerText = 0;
+// selecting the numbers as buttons
+let numberBtn = document.querySelectorAll(".button");
+numberBtn.forEach((btn) =>
+  btn.addEventListener("click", () => {
+    number.innerText = btn.innerText;
+  })
+);

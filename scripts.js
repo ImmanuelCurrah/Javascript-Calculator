15ed35f30d8caeafcd2b399da9f2fb97abb2c336
Immanuel Currah <immanueli.currah@gmail.com>
2022-05-03 13:00:19 -0400

    basic logic is on its way

--- scripts.js
+++ scripts.js
@@ -1,10 +1,22 @@
 // set the starting number to 0
 let number = document.getElementById("number");
-number.innerText = 0;
+number.innerText = null;
+
+//set an empty variable to store the number we desire
+let prevNum = 0;
+
 // selecting the numbers as buttons
 let numberBtn = document.querySelectorAll(".button");
 numberBtn.forEach((btn) =>
   btn.addEventListener("click", () => {
-    number.innerText = btn.innerText;
+    if (+btn.innerText) {
+      number.innerText += btn.innerText;
+    } else if (!+btn.innerText && btn.innerText !== "=") {
+      prevNum = number.innerText;
+      number.innerText = null;
+      console.log(prevNum);
+    } else if (btn.innerText === "=") {
+      number.innerText = +prevNum + +number.innerText;
+    }
   })
 );
